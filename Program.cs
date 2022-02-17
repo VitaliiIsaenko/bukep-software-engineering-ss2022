665a50f11749453269d052150646468d5a5d2dfb
evoshny <blackford0@yandex.ru>
2022-02-17 20:47:47 +0300

    добавил 1 лабораторную работу

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Хотите ли вы жить у моря?");
+string answer = Console.ReadLine();
+bool liveBySea;
+if (answer == "да")
+{
+    liveBySea = true;
+}
+else
+{
+    liveBySea = false; 
+}
