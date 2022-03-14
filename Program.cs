c8c8d575308a32dc08c77d48a3f0f520a685efce
Алексей <aleksej-zvonarev05@mail.ru>
2022-03-14 15:49:38 +0300

    Сделал лабораторную работу

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Хотите ли вы жить у моря?") ;
+string? answer = Console.ReadLine() ; 
+bool LiveBySea ;
+if (answer == "да")
+{
+    LiveBySea = true;
+}
+else
+{
+    LiveBySea = false;
+}
+Console.WriteLine(LiveBySea) ;
