41e10a89449ae5c2693c10a1928fb38b1b66e383
bezuglov <dimabezuglov501@gmail.com>
2022-03-14 21:15:15 +0300

    комментарий к коммиту

--- CountriesRecommendation/CountriesRecommendation/Program.cs
+++ CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -1,24 +1,39 @@
-﻿namespace CountriesRecommendation;
+﻿using CountriesRecommendation.Helpers;
+
+namespace CountriesRecommendation;
 
 public class Program
-{
-    public void Main()
     {
-        Console.WriteLine("Хотите ли вы жить у моря?");
-        string answer = Console.ReadLine();
+    private const int V = 1;
 
-        bool liveBySea;
-
-        if (answer == "да")
+    public static void Main()
         {
-            liveBySea = true;
-        }
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            string answer = Console.ReadLine();
 
-        else
-        {
-            liveBySea = false;
-        }
+            bool liveBySea;
 
-        Console.WriteLine(liveBySea);
+            if (answer == "да")
+            {
+                liveBySea = true;
+            }
+
+            else
+            {
+                liveBySea = false;
+            }
+
+            Console.WriteLine(liveBySea);
+            Country[] countries = CountriesCsvReader.ReadCountries("bezuglov.csv");
+            CountriesCsvReader.ReadCountries("bezuglov.csv");
+            
+            if (countries[0].HasSea)
+            {
+                Console.WriteLine(countries[0].ToString());
+            }
+            else
+            {
+                Console.WriteLine(countries[1].ToString());
+            } 
+        }
     }
-}
