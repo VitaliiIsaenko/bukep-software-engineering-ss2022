389b8eacdc9b2ab08bf7b758faa40e9cd9968bc7
evoshny <blackford0@yandex.ru>
2022-03-19 13:41:53 +0300

    добавил 2 лабораторную работу

---
+++ CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -0,0 +1,29 @@
+﻿using CountriesRecommendation.Helpers;
+
+namespace CountriesRecommendation;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Хотите ли вы жить у моря?");
+        string? answer = Console.ReadLine();
+
+        bool liveBySea;
+        liveBySea = answer == "да";
+        Console.WriteLine(liveBySea);
+        Country[] countries = CountriesCsvReader.ReadCountries("Shihantsov.csv");
+        CountriesCsvReader.ReadCountries("Shihantsov.csv");
+        
+        if (countries[0].HasSea)
+        {
+            Console.WriteLine(countries[0].ToString());
+        }
+
+        else
+        {
+            Console.WriteLine(countries[1].ToString());
+        }
+    }
+}
+
