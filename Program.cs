45ad33beee44fe51a3a621afc4fcef694a53f1df
Владимир <vvvv-gunch2000@yandex.ru>
2022-04-02 18:12:32 +0300

    Gunchenko

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Хотите ли вы жить у моря?");
+string answer = Console.ReadLine();
+bool liveBySea;
+if (answer == "да")
+{
+    liveBySea = true;
+}
+else
+{
+    liveBySea = false;
+}
+Console.WriteLine(liveBySea);
