1670c63e36b8b5a3e4ab45b27aac0efa3fcc4496
Vladimir <99183335+VladimirGunch@users.noreply.github.com>
2022-04-03 12:37:41 +0300

    Add files via upload

---
+++ lab-2.cs
@@ -0,0 +1,31 @@
+﻿using CountriesRecommendation.Helpers;
+
+namespace CountriesRecommendation;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Хотите ли вы жить у моря?");
+        string answer = Console.ReadLine();
+
+        bool liveBySea;
+
+        if (answer == "да")
+        {
+            liveBySea = true;
+        }
+
+        else
+        {
+            liveBySea = false;
+        }
+
+        Console.WriteLine(liveBySea);
+    }
+}
+Country[] countries = CountriesCsvReader.ReadCountries("Gunchenko.csv");
+CountriesCsvReader.ReadCountries(Gunchenko.csv)
+if (countries[0].HasSea)
+Console.WriteLine = countries[0].ToString()
+liveBySea = answer == "да"
