be38b1273393b95d5efd058985068d6642ab4722
whateto <rendesvald@gmail.com>
2022-04-13 14:47:06 +0300

    Добавить лабораторную работу

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Хотите ли Вы жить у моря?");
+string answer = Console.ReadLine();
+bool liveBySea;
+if (answer =="да")
+{
+liveBySea = true;
+}
+else
+{
+liveBySea = false;
+}
+Console.WriteLine(liveBySea);
+
+
