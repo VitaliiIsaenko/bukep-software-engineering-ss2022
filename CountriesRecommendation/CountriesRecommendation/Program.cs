e32c0f929c894b3102f3b2f17986eba2408bfcb1
abondarev1 <ant.bond02@mail.ru>
2022-04-27 14:59:16 +0300

    lab

--- CountriesRecommendation/CountriesRecommendation/Program.cs
+++ CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -1,24 +1,28 @@
-﻿namespace CountriesRecommendation;
+using CountriesRecommendation.Helpers;
+namespace CountriesRecommendation;
 
 public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Хотите ли вы жить у моря?");
-        string answer = Console.ReadLine();
+        var answer = Console.ReadLine();
 
-        bool liveBySea;
-
-        if (answer == "да")
-        {
-            liveBySea = true;
-        }
-
-        else
-        {
-            liveBySea = false;
-        }
+        var liveBySea = answer == "да";
 
         Console.WriteLine(liveBySea);
+
+        Country[] countries = CountriesCsvReader.ReadCountries("bondarev.csv");
+    
+        if (liveBySea == true) {
+        if (countries[0].HasSea) {
+            Console.WriteLine(countries[0].ToString());
+        }
+        if (countries[1].HasSea) {
+            Console.WriteLine(countries[1].ToString());
+            }
+        } else {
+            Console.WriteLine(countries[0].ToString());
+        }
     }
 }
