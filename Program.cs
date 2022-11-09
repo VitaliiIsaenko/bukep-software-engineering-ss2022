4a8bb61cae2d6067b4bc03bbe129b4d97baca4d4
Даниил <densavelev1337@gmail.com>
2022-11-09 19:49:53 +0300

    лабораторная 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Хотите ли вы жить у моря?");
+string answer = Console.ReadLine();
+bool liveBySea;
+if (answer == "да")
+{
+    liveBySea = true;
+}
+else
+{
+    liveBySea = false;
+}
+Console.WriteLine(liveBySea);
