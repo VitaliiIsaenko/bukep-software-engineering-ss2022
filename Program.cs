bf35e24365a5d604a1d6956735e8c17194b78af5
Roman WwSeaSeR <k0v223roma@yandex.ru>
2022-11-09 18:15:28 +0300

    Добавить файл с информацией о странах

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.dWriteLine ("Хотите ли вы жить у моря?");
+string answer = Console.ReadLine ();
+bool liveBySea;
+if (ansfer == "да")
+{
+    liveBySea = true;
+}
+esle
+{
+    liveBySea =  false;
+}
