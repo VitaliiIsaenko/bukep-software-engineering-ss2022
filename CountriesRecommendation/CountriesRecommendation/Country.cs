8b8691e025447023de42bc736cb1d71f85e7902c
Semin Daniil <daniilsemin258@gmail.com>
2022-12-23 16:38:54 +0300

    Добавить файл с информацией о странах

---
+++ CountriesRecommendation/CountriesRecommendation/Country.cs
@@ -0,0 +1,32 @@
+﻿using CountriesRecommendation.Helpers;
+
+namespace CountriesRecommendation;
+
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Хотите ли вы жить у моря");
+    string? answer = Console.ReadLine();
+    bool liveBySea;
+    liveBySea = answer =="да";
+    UserPreferences preferences = new UserPreferences();
+    preferences.SetLiveBySea(liveBySea);
+    string[][] countriesInfo = CsvReader.Read("Semin.csv");
+    Country[] countries = new Country[countriesInfo.Length];
+    for (int i = 0; i < countriesInfo.Length; i++)
+    {
+      string[] countryInfo = countriesInfo[i];
+      countries[i] = new Country(countryInfo[0], long.Parse(countryInfo[1]), int.Parse(countryInfo[2]),countryInfo[3] == "да");
+    } 
+      foreach(Country country in countries)
+      {
+        if (preferences.Satisfied(country))
+        {
+          Console.WriteLine(country.Name);
+          return;
+        }
+      }
+      Console.WriteLine("Извините, мы не смогли подобрать страну с такими параметрами");
+  }
+
